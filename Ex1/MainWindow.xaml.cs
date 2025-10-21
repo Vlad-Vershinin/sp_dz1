75983f7fbf4014c6c13fcd336ae32beeec55cb28
Vlad-Vershinin <vladversinin676@gmail.com>
2025-10-22 00:21:33 +0500

    init

---
+++ Ex1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace Ex1;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        ThreadInfoText.Text = $"Главный поток (UI): {Thread.CurrentThread.ManagedThreadId}";
+    }
+
+    private async void StartNotepadButton_Click(object sender, RoutedEventArgs e)
+    {
+        StatusText.Dispatcher.Invoke(() => StatusText.Text = "Запуск Notepad...");
+
+        try
+        {
+            var processStartInfo = new ProcessStartInfo
+            {
+                FileName = "notepad.exe",
+                UseShellExecute = true
+            };
+
+            var process = Process.Start(processStartInfo);
+            
+            await Task.Delay(3000);
+
+            var realProcess = Process.GetProcessesByName("Notepad")[0];
+            realProcess.Kill();
+        }
+        catch (Exception ex)
+        {
+            StatusText.Dispatcher.Invoke(() => StatusText.Text = $"Ошибка: {ex.Message}");
+        }
+    }
+}
---
+++ Ex2/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+
+namespace Ex2;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void OnButtonClick(object sender, RoutedEventArgs e)
+    {
+        using (Process process = new Process())
+        {
+            var info = process.StartInfo;
+
+            info.FileName = "cmd.exe";
+            info.Arguments = "/c dir";
+            info.UseShellExecute = false;
+            info.RedirectStandardOutput = true;
+            info.RedirectStandardError = true;
+            info.CreateNoWindow = true;
+            info.WorkingDirectory = Environment.CurrentDirectory;
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            info.StandardOutputEncoding = Encoding.GetEncoding(866);
+            info.StandardErrorEncoding = Encoding.GetEncoding(866);
+
+            try
+            {
+                process.Start();
+
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+                
+                process.WaitForExit();
+
+                if (!string.IsNullOrEmpty(error))
+                {
+                    Dispatcher.Invoke(() => Output.Text = "Error:\n" + error);
+                }
+                else
+                {
+                    Dispatcher.Invoke(() => Output.Text = "Result:\n" + output);
+                }
+            }
+            catch (Exception ex)
+            {
+                Output.Text = ex.Message;
+            }
+        }
+    }
+}
